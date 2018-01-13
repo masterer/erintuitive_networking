5b66acd3092463826f7d4da2e5fc61c647947bf9
Erin M. Masterson <erin.margot.masterson@gmail.com>
2018-01-13 01:37:06 -0500

    trying again

--- basic_networking.js
+++ basic_networking.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
-const SetInterval = require('set-interval');
+//const SetInterval = require('set-interval');
 
 require('es6');
 
